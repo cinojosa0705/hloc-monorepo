facb1ae917fcff312dcd0ad81fdfe8496c31226a
Thales <81639334+cinojosa0705@users.noreply.github.com>
2024-02-05 01:08:08 -0500

    Update const.ts

--- websocket/src/const.ts
+++ websocket/src/const.ts
@@ -1,3 +1,12 @@
+const pgp = pgPromise();
+const db = pgp({
+  host: "containers-us-west-155.your-host.com",
+  port: 7917,
+  database: "your-database",
+  user: "postgres",
+  password: "your-password",
+});
+
 export const ApiKeyArray = [
     '239asH3yR2K2onO0S21-example-api-key',
 ]
